8a43829ea1e3a5c077ce3448167ec0f4d6dfcad7
2201018 <148177579+2201018@users.noreply.github.com>
2025-04-15 03:06:17 +0000

    try3

--- public/try3/inputuser.php
+++ public/try3/inputuser.php
@@ -9,7 +9,7 @@
     <div class="login-box">
       <h2>
 <?php
-if(mb_ereg('^[ぁ-んァ-ヶ一-龠々]{20}$', $_GET['username'])==false)
+if(mb_ereg('^[ぁ-んァ-ヶ一-龠々]{1,20}$', $_GET['username'])==false)
   {
     die("20文字以内の日本語で入力してください");
   }else{
@@ -17,14 +17,14 @@ if(mb_ereg('^[ぁ-んァ-ヶ一-龠々]{20}$', $_GET['username'])==false)
     echo "名前：" . $_GET['username'] . "<br>";
   }
 
-if(mb_ereg('^[ぁ-んァ-ヶ一-龠々]{50}$', $_GET['username'])==false)
+if(mb_ereg('^[ぁ-んァ-ヶ一-龠々]{1,50}$', $_GET['username'])==false)
   {
     die("50文字以内の日本語で入力してください");
   }else{
     echo "住所：" . $_GET['useraddress']. "<br>";
   }
 
-  if(mb_ereg('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}{100}$', $_GET['usermail'])==false)
+  if(mb_ereg('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}{1,100}$', $_GET['usermail'])==false)
   {
     die("100文字以内で入力してください");
   }else{
