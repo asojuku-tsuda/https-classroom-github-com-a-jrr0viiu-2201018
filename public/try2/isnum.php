43b17b3612394f6280e3087823a926496db9f81f
2201018 <148177579+2201018@users.noreply.github.com>
2025-04-15 01:28:55 +0000

    数値

--- public/try2/isnum.php
+++ public/try2/isnum.php
@@ -10,8 +10,12 @@
       <h2>
 <?php
 
-
-echo "入力された数字は： " . $_GET['indata'];
+if(mb_ereg('^[0-9]+$', $indata)==false)
+{
+  die(”数値を入力してください”);
+}else{
+  echo "入力された数字は： " . $indata;
+}
 ?>
     </h2>
     </div>
